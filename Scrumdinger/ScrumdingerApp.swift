7f6b07c9c390a38271dea8d13a9b125ac028d3dd
Albert <gaudicos61@gmail.com>
2021-05-01 22:45:37 +0800

    Meeting View UI for Scrumdinger

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Saski Skye on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
