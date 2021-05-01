d0d79f61cac8cd9be62f220a0ebaa4e7f0447b7e
Albert <gaudicos61@gmail.com>
2021-05-01 22:50:48 +0800

    Add DailyScrum model

---
+++ Scrumdinger/DailyScrum.swift
@@ -0,0 +1,15 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Saski Skye on 5/1/21.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
