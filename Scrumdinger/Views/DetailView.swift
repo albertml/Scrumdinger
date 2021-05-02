160997a7ac8d222143782944eb9cd3aa68cfe2b0
Albert <gaudicos61@gmail.com>
2021-05-02 14:59:11 +0800

    Display list of meeting attendees

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -32,6 +32,15 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .ignore)
             }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("person"))
+                        .accessibilityValue(Text(attendee))
+                    
+                }
+            }
         }
         .listStyle(InsetGroupedListStyle())
     }
